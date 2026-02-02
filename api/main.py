a3b3e6515651e1c9961c62e3bd334698e6fd51fb
Kirsten Melissa ✨ <103148299+ofthewildfire@users.noreply.github.com>
2026-02-02 15:16:39 +0200

    Added .venv and main.py test for api with fastapi

---
+++ api/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/check")
+async def check():
+    return {"content:": "I work, from Next.js too... how cool?"}
---
+++ client/app/check/page.tsx
@@ -0,0 +1,6 @@
+export default async function CheckPage() {
+  const res = await fetch("http://127.0.0.1:8000/check");
+  const message = await res.json();
+  const return_msg = Object.values(message);
+  return return_msg;
+}
